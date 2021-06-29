9c3855ea317934bdbb1f57ca62267fe587aefc48
VaranasiNavya <VaranasiNavya@DESKTOP-4946J1H>
2021-06-29 11:01:10 +0530

    commit

---
+++ employeeexercise/src/main/java/com/infy/EmployeeexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeexerciseApplication.class, args);
+	}
+
+}
