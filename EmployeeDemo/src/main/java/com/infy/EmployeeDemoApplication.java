f7e79cc3864dcb517bf3cfe471b27ba160dc2023
VaranasiNavya <VaranasiNavya@DESKTOP-4946J1H>
2021-07-06 21:58:10 +0530

    securing

--- EmployeeDemo/src/main/java/com/infy/EmployeeDemoApplication.java
+++ EmployeeDemo/src/main/java/com/infy/EmployeeDemoApplication.java
@@ -2,6 +2,7 @@ package com.infy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
@@ -13,6 +14,11 @@ public class EmployeeDemoApplication implements WebMvcConfigurer {
 		SpringApplication.run(EmployeeDemoApplication.class, args);
 	}
 	
+	//CORS
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST");
+	}
 	//matrix paramenters
 	@Override
 	public void configurePathMatch(PathMatchConfigurer configurer) {
